9b76c13d5663bd8a41fab4dfeacdffa9be9fab36
mikera <mike.r.anderson.13@gmail.com>
2012-12-10 23:42:56 +0800

    doseq-indexed test

--- src/test/clojure/mikera/cljutils/test_loops.clj
+++ src/test/clojure/mikera/cljutils/test_loops.clj
@@ -8,3 +8,13 @@
       (for-loop [i 0 (< i 10) (inc i)]
                 (swap! c inc))
       (is (= @c 10)))))
+
+(deftest test-doseq-indexed
+  (testing "ten iterations"
+    (let [c (atom 0)
+          ic (atom 0)]
+      (doseq-indexed [x [1 2 3 4] i]
+                (swap! c + x)
+                (swap! ic + i))
+      (is (= @c 10))
+      (is (= @ic 6)))))
