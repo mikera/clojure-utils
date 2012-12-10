1c25aea814de6db514fd25aa2d0320f90bbd60f4
mikera <mike.r.anderson.13@gmail.com>
2012-12-10 16:25:50 +0800

    error handling

--- src/test/clojure/mikera/cljutils/test_error.clj
+++ src/test/clojure/mikera/cljutils/test_error.clj
@@ -1,6 +1,6 @@
 (ns mikera.cljutils.test-error
-  (:use clojure.test
-  (:use [mikera.cljutils error])))
+  (:use clojure.test)
+  (:use [mikera.cljutils error]))
 
 (deftest test-error
   (testing "Error"
