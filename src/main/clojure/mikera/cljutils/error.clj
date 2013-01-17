5b60718e5cf4e8c32a70973a78374f908c053413
mikera <mike.r.anderson.13@gmail.com>
2013-01-17 11:48:19 +0800

    Doc updates

--- src/main/clojure/mikera/cljutils/error.clj
+++ src/main/clojure/mikera/cljutils/error.clj
@@ -6,6 +6,7 @@
     `(throw (mikera.cljutils.Error. (str ~@vals)))))
 
 (defmacro TODO
-  "Throws a TODO error"
+  "Throws a TODO error. This ia a useful macro as it is easy to search for in source code, while
+   also throwing an error at runtime if encountered."
   ([]
     `(error "TODO: Not yet implemented")))
