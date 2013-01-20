e43f04886e3c19ed2181447e6dbe39f2f7c39826
mikera <mike.r.anderson.13@gmail.com>
2013-01-20 11:18:40 +0800

    try-or macro

--- src/main/clojure/mikera/cljutils/error.clj
+++ src/main/clojure/mikera/cljutils/error.clj
@@ -10,3 +10,11 @@
    also throwing an error at runtime if encountered."
   ([]
     `(error "TODO: Not yet implemented")))
+
+(defmacro try-or 
+  "Trys expressions in sequence until one produces a result that is neither false nor an exception.
+   Useful for providing a default value in the case of errors."
+  ([exp & alternatives]
+     (if-let [as (seq alternatives)] 
+       `(or (try ~exp (catch Throwable t# (try-or ~@as))))
+       exp)))
