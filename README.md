858a0a9ff089c907e4b6f68114bd1e53b886ca99
mikera <mike.r.anderson.13@gmail.com>
2013-02-11 13:05:53 +0800

    Add license info

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ Of particular Interest:
  - mikera.cljutils.Clojure - Class of utility functions for calling Clojure from Java
  - mikera.cljutils.namepace - Clojure functions for merging / managing namespaces
  - mikera.cljutils.loops - Extra looping constructs and macros. Handy if you need them.
+ 
 
--- src/main/clojure/mikera/cljutils/expression.clj
+++ src/main/clojure/mikera/cljutils/expression.clj
@@ -5,6 +5,16 @@
   "Evaluates a constant expression at compile time" 
   (eval body))
 
+
+
+;   Copyright (c) Chris Houser, Dec 2008. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+;   which can be found in the file CPL.TXT at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
 (defn expression-info
  "Uses the Clojure compiler to analyze the given s-expr. Returns
  a map with keys :class and :primitive? indicating what the 
