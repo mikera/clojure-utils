24470b185e9348a04fd9ac025bec6fd781d6c009
mikera <mike.r.anderson.13@gmail.com>
2013-02-17 22:14:27 +0800

    and* function

--- src/test/clojure/mikera/cljutils/test_logic.clj
+++ src/test/clojure/mikera/cljutils/test_logic.clj
@@ -9,3 +9,12 @@
     (is (xor 1))
     (is (not (xor)))
     (is (not (xor true false false true false false true true)))))
+
+(deftest test-and*
+  (testing "AND"
+    (is (and* 1 2))
+    (is (and* 1 1 1 1))
+    (is (and* 1))
+    (is (and*))
+    (is (not (and* nil)))
+    (is (not (and* 1 1 1 1 1 1 nil 1 1 1 1)))))
