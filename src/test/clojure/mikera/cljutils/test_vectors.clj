aabafd9bcef8de04f5480331baac4f4622c6dae4
mikera <mike.r.anderson.13@gmail.com>
2013-04-09 16:59:35 +0800

    more tests

--- src/test/clojure/mikera/cljutils/test_vectors.clj
+++ src/test/clojure/mikera/cljutils/test_vectors.clj
@@ -8,3 +8,9 @@
   (is (= [] (vector-without [1] 0))))
 
 
+(deftest test-vector-without
+  (let [a 1 b 2 c 3]
+    (= [a b] (vector-without [a b c] 2))
+    (= [a c] (vector-without [a b c] 1))
+    (= [b c] (vector-without [a b c] 0))
+    (= [] (vector-without [a] 0))))
