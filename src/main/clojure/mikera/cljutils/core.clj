0571bf20c79c8ba5c2a986bb17f016768d39dbcd
mikera <mike.r.anderson.13@gmail.com>
2013-04-26 23:24:57 +0800

    docs

--- src/main/clojure/mikera/cljutils/core.clj
+++ src/main/clojure/mikera/cljutils/core.clj
@@ -2,7 +2,9 @@
  
  (defn apply-kw
    "Applies a function to a set of arguments, where the last argument is a map of 
-    additional keyword arguments."
+    additional keyword arguments.
+
+    Directly provided keywords will override those in the final argument map."
    [f & args]
    {:pre [(map? (last args))]}
    (if-let [kvs (seq (butlast args))]
