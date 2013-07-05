01edd8b7d699d51ad5f3b95c97a61c7c1ddf2cca
mikera <mike.r.anderson.13@gmail.com>
2013-07-05 19:58:49 +0100

    more and-as-> tests

--- src/test/clojure/mikera/cljutils/test_macros.clj
+++ src/test/clojure/mikera/cljutils/test_macros.clj
@@ -6,7 +6,24 @@
   (testing "Basic results"
     (is (= 1 (and-as-> 1 x)))
     (is (= nil (and-as-> nil x)))
-    (is (= 1 (and-as-> nil x 1))))
+    (is (= nil (and-as-> nil x 
+                         x 
+                         x)))
+    (is (= 1 (and-as-> nil x 
+                       1))))
   (testing "Sequential results"
-    (is (== 3 (and-as-> 1 x (inc x) (inc x))))))
+    (is (== 3 (and-as-> 1 x 
+                        (inc x) 
+                        (inc x))))
+    (is (== nil (and-as-> 1 x 
+                          (inc x) 
+                          nil 
+                          (inc x)))))
+  (testing "Falsiness"
+    (is (== false (and-as-> 1 x 
+                            false 
+                            true)))
+    (is (== true (and-as-> false x 
+                           true 
+                           false)))))
 
