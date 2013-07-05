31c942b1e4663d776441883b1d9f7d7ac63834e0
mikera <mike.r.anderson.13@gmail.com>
2013-07-05 19:58:41 +0100

    fix typo and formatting

--- src/main/clojure/mikera/cljutils/macros.clj
+++ src/main/clojure/mikera/cljutils/macros.clj
@@ -1,12 +1,10 @@
 (ns mikera.cljutils.macros)
 
-
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* true)
 
-
 (defmacro and-as-> 
-  "Similar to as->, except wraps an implicit and around ech step: returns nil/false
+  "Similar to as->, except wraps an implicit and around each step: returns nil/false
    as soon as any expression returns nil/false."
   ([expr sym & body]
   `(as-> ~expr ~sym
