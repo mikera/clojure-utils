97bdf390eaa6da2eafc11a65e3a4113a9fb9f79c
mikera <mike.r.anderson.13@gmail.com>
2013-08-17 00:21:37 +0800

    add mouse position handling namespace

---
+++ src/main/clojure/mikera/cljutils/mouse.clj
@@ -0,0 +1,24 @@
+(ns mikera.cljutils.mouse
+  (:import [java.awt MouseInfo PointerInfo Point]))
+
+(defn- mouse-point 
+  (^Point []
+    (.getLocation (MouseInfo/getPointerInfo))))
+
+(defn mouse-pos 
+  "Polls the current mouse position relative to the screen"
+  ([]
+    (let [pt (mouse-point)]
+      [(long (.x pt)) (long (.y pt))])))
+
+(defn mouse-x 
+  "Polls the current mouse x-position relative to the screen"
+  (^long []
+    (let [pt (mouse-point)]
+      (long (.x pt)))))
+
+(defn mouse-y 
+  "Polls the current mouse y-position relative to the screen"
+  (^long []
+    (let [pt (mouse-point)]
+      (long (.y pt)))))
