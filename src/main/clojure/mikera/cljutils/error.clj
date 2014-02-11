2279e264af0a828783a98287575295a0780074cf
mikera <mike.r.anderson.13@gmail.com>
2014-02-11 20:40:35 +0800

    refactor error messages

--- src/main/clojure/mikera/cljutils/error.clj
+++ src/main/clojure/mikera/cljutils/error.clj
@@ -6,8 +6,8 @@
 (defmacro error
   "Throws an error with the provided message(s). This is a macro in order to try and ensure the 
    stack trace reports the error at the correct source line number."
-  ([& vals]
-    `(throw (mikera.cljutils.Error. (str ~@vals)))))
+  ([& messages]
+    `(throw (mikera.cljutils.Error. (str ~@messages)))))
 
 (defmacro error?
   "Returns true if executing body throws an error, false otherwise."
