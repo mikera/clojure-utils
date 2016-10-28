e6ca7a69df8c9df7f235ad310c2944c5f72afe0c
Mike <mike@mikera.net>
2016-10-28 19:12:21 +0800

    Extra loops tests

--- src/test/clojure/mikera/cljutils/test_loops.clj
+++ src/test/clojure/mikera/cljutils/test_loops.clj
@@ -1,16 +1,17 @@
 (ns mikera.cljutils.test-loops
   (:use clojure.test)
-  (:use [mikera.cljutils loops]))
+  (:use [mikera.cljutils loops])
+  (:require [mikera.cljutils.error :refer [error]]))
 
 (deftest test-for-loop
-  (testing "ten iterations"
+  (testing "Ten iterations"
     (let [c (atom 0)]
       (for-loop [i 0 (< i 10) (inc i)]
                 (swap! c inc))
       (is (= @c 10)))))
 
 (deftest test-doseq-indexed
-  (testing "ten iterations"
+  (testing "doseq-indexed with small vector"
     (let [c (atom 0)
           ic (atom 0)]
       (doseq-indexed [x [1 2 3 4] i]
@@ -27,10 +28,13 @@
              (swap! r + i)
              (swap! vs conj v))
       (is (= #{:a :b :c} @vs))
-      (is (== 3 @r)))))
+      (is (== 3 @r))))
+  (testing "Empty collection"
+    (dovec [v []] (error "Shouldn't happen!"))
+    (dovec [v nil] (error "Shouldn't happen!"))))
 
 (deftest test-eager-map
-  (testing "eager map"
+  (testing "Eager map"
     (is (= [1 2 3] (eager-map inc [0 1 2])))
     (is (nil? (eager-map inc [])))
     (is (nil? (eager-map inc nil))))) 
